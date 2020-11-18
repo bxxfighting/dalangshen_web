003735040949b6c2f0cbd865509743f28d4d9036
卜星星 <boxingxing@limikeji.com>
2020-11-18 10:19:12 +0800

    认证页面

---
+++ src/views/asset/auth.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-loading="loading" class="app-container">
+    <el-card>
+      <div slot="header">
+        <span> 请输入密钥 </span>
+      </div>
+      <div>
+        <el-form ref="form" :model="form">
+          <el-form-item label="密钥">
+            <el-input v-model="form.key" />
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="go">
+              进入
+            </el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Auth',
+  data() {
+    return {
+      loading: false,
+      form: {
+        key: '',
+        show: false,
+        status: 'create'
+      }
+    }
+  },
+  methods: {
+    go() {
+    }
+  }
+}
+</script>
